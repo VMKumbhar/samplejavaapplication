87165bc515e1fb236e519fc5a2f685c28a7300b6
VMKumbhar <46930216+VMKumbhar@users.noreply.github.com>
2024-06-06 11:23:16 +0530

    Create ExampleTest.java

---
+++ src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAdd() {
+        Example example = new Example();
+        assertEquals(5, example.add(2, 3));
+    }
+}
