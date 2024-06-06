85750fba9484ef450e65ed375a1f4b3065fdc3b1
VMKumbhar <46930216+VMKumbhar@users.noreply.github.com>
2024-06-06 13:00:47 +0530

    Update ExampleTest.java

--- src/test/java/com/example/ExampleTest.java
+++ src/test/java/com/example/ExampleTest.java
@@ -10,9 +10,4 @@ public class ExampleTest {
         Example example = new Example();
         assertEquals(5, example.add(2, 3));
     }
-    @Test
-    public void testAdd() {
-        Example example = new Example();
-        assertEquals(0, example.add(0, 0));
-    }
 }
