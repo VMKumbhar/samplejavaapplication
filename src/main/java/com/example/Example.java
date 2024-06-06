d1d0590c22e45bbb44d3114c65ff0e78180b9844
VMKumbhar <46930216+VMKumbhar@users.noreply.github.com>
2024-06-06 12:22:37 +0530

    Update Example.java

--- src/main/java/com/example/Example.java
+++ src/main/java/com/example/Example.java
@@ -2,11 +2,8 @@ package com.example;
 
 public class Example {
 
-    // Ignore all issues in this method
-    @SuppressWarnings("squid:S00112")
     public void methodWithIgnoredIssues() {
-        // Issue: This statement has no effect
-        int x = 10; // This statement is just an example and will be ignored by SonarQube
+        int x = 10;
     }
 
     public int add(int a, int b) {
