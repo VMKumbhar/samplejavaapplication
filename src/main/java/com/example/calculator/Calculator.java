1db58643d8af4ca657c99ebcf16414432d46d95e
VMKumbhar <46930216+VMKumbhar@users.noreply.github.com>
2024-06-06 18:47:35 +0530

    Update Calculator.java

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -8,12 +8,13 @@ public class Calculator {
     }
     
     // Ignore all issues in this method
-    
+    // BEGIN-NOSCAN
     public void methodWithIgnoredIssues() {
         // Issue: This statement has no effect
         int x = 10; // This statement is just an example and will be ignored by SonarQube
     }
-    
+    // END-NOSCAN
+
     public int subtract(int a, int b) {
         return a - b;
     }
